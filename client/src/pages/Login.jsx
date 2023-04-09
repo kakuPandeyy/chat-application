8b18c53d02f530324f76b75c856117449163dcd8
abhishek pandey <kakupandey2017@gmail.com>
2023-04-09 19:44:19 +0530

    v2

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 
-import React ,{ useEffect, useState } from 'react';
+import React ,{  useState } from 'react';
 import FormContainer from '../style/FromContainer';
 import logo from "../assets/logo.png";
 import { ToastContainer,toast } from 'react-toastify';
--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-application-server-z3we.onrender.com";
 export const  registerRoute = `${host}/api/auth/register`
 export const  logInRouter = `${host}/api/auth/login`
 export const  setAvatarRouter = `${host}/api/auth/setAvatar`
